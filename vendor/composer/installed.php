0fd57498c17b8d37fef5f38357e853399bf9b0cd
Marcelo Alba <malba@gcgiglobal.com>
2022-09-05 11:00:33 -0400

    init plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'db278e397714309491cad0221c13bdfd2a8b8e2e',
+    'name' => 'marceloalbam/tem-edsi',
+  ),
+  'versions' => 
+  array (
+    'marceloalbam/tem-edsi' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db278e397714309491cad0221c13bdfd2a8b8e2e',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.7.1',
+      'version' => '3.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1359e176e9307e906dc3d890bcc9603ff6d90619',
+    ),
+    'wp-coding-standards/wpcs' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f328bcafd97377e8e5e5d7b244d5ddbf301a3a5c',
+    ),
+  ),
+);
